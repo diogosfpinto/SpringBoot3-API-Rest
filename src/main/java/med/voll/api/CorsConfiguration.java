dd99ebd439de3422e33746644ffa79414a30240e
diogosfpinto <diogo.sfpinto@gmail.com>
2023-04-26 21:03:14 -0400

    first commit

---
+++ src/main/java/med/voll/api/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package med.voll.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/** Classe que corrige o erro de CORS para acesso a API
+ * */
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
