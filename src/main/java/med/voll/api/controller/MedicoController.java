f86f94bdeaa9c31242f11a6ec26cabeba3c036ce
diogosfpinto <diogo.sfpinto@gmail.com>
2023-07-05 21:31:32 -0400

    Add listar medico endpoint

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListaMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +22,16 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListaMedico> listar(){
+        return repository.findAll().stream()
+                .map(medico -> new DadosListaMedico(
+                        medico.getNome(),
+                        medico.getEmail(),
+                        medico.getCrm(),
+                        medico.getEspecialidade()))
+                .toList();
     }
 }
